219c4aa74fea0c97be3e798dc24e1db06660db2f
Leticia Missias <leticia.m.icm@gmail.com>
2021-11-11 12:38:01 -0300

    adiciona exercício 3

--- 01-fundamentos/bloco-04-introdução-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bonus/script.js
+++ 01-fundamentos/bloco-04-introdução-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bonus/script.js
@@ -11,7 +11,7 @@ for (let i=1; i<numbers.length; i+=1){
   }
 
 };
-console.log(numbers)
+console.log(numbers);
 console.log("\n")
 
 // Exercicio 2 - Coloque o array em ordem decrescente
@@ -24,5 +24,20 @@ for(let i=1; i<numbers.length; i+=1){
       numbers[i2]=position;
     }
   }
+};
+console.log(numbers);
+
+//Exercicio 3 - Crie um novo array a partir do primeiro, onde cada elemento representa a multiplicação do elemento atual vezes o elemento seguinte. E se não houver elemento seguinte, o elemtno é multiplicado por 2.
+
+let numbers1 = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]; //Foi criado outro array (com os mesmos dados), somente para não dar conflito na hora de imprimir os resultados.
+
+let newNumbers=[];
+for (let i=0; i<numbers1.length; i+=1){
+  if(numbers1[i]==numbers1[numbers1.length-1]){
+    newNumbers.push(numbers1[i]*2)
+  }
+  else{
+  newNumbers.push(numbers1[i]*numbers1[i+1])
 }
-console.log(numbers)
+}
+console.log(newNumbers)
