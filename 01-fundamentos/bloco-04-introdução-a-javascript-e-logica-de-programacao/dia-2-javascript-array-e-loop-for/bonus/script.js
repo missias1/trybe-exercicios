d5cab5664d977e1af64b1548ffe492085a577aac
Leticia Missias <leticia.m.icm@gmail.com>
2021-11-11 12:23:06 -0300

    adiciona exercicios 1 e 2 da parte bonus

---
+++ 01-fundamentos/bloco-04-introdução-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bonus/script.js
@@ -0,0 +1,28 @@
+//Exercicio 1 - Coloque o array em ordem crescente
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let i=1; i<numbers.length; i+=1){
+  for(let i2=0; i2<i; i2+=1){
+    if(numbers[i] < numbers[i2]){
+      let position=numbers[i];
+      numbers[i]=numbers[i2];
+      numbers[i2]=position;
+    }
+  }
+
+};
+console.log(numbers)
+console.log("\n")
+
+// Exercicio 2 - Coloque o array em ordem decrescente
+
+for(let i=1; i<numbers.length; i+=1){
+  for (let i2=0; i2<i; i2+=1){
+    if(numbers[i]>numbers[i2]){
+      let position=numbers[i];
+      numbers[i]=numbers[i2];
+      numbers[i2]=position;
+    }
+  }
+}
+console.log(numbers)
