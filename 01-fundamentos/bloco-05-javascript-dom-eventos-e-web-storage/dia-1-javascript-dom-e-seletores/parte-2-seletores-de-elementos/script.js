b7daeddd178f32805377ad518cf450222c82a841
Leticia Missias <leticia.m.icm@gmail.com>
2021-11-21 02:55:44 -0300

    colocando cores com getElementBy e querySelector

---
+++ 01-fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/parte-2-seletores-de-elementos/script.js
@@ -0,0 +1,15 @@
+document.querySelector("header").style.backgroundColor="rgb(0, 186, 88)";
+
+document.body.style.backgroundColor="f1f1f1";
+
+document.querySelector(".emergency-tasks").style.backgroundColor="rgb(234, 141, 115)"
+
+document.getElementsByTagName("h3")[0].style.backgroundColor="rgb(194, 21, 225)"
+document.getElementsByTagName("h3")[1].style.backgroundColor="rgb(194, 21, 225)"
+
+document.getElementsByClassName("no-emergency-tasks")[0].style.backgroundColor="rgb(255, 206, 88)"
+
+document.querySelector(".no-emergency-tasks h3").style.backgroundColor="black"
+document.querySelectorAll(".no-emergency-tasks h3")[1].style.backgroundColor="black"
+
+document.getElementById("footer-container").style.backgroundColor="rgb(0, 75, 49)"
