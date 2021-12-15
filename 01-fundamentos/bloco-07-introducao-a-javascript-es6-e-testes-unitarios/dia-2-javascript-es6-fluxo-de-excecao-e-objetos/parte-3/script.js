f2f2c543cf23ff4c788eae3f73dec890355699d0
Leticia Missias <leticia.m.icm@gmail.com>
2021-12-15 16:02:31 -0300

    exercicios parte 3, do 7 ao 8

--- 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/parte-3/script.js
+++ 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/parte-3/script.js
@@ -17,36 +17,36 @@ const lesson3 = {
   professor: 'Maria Clara',
   turno: 'noite',
 };
-
+//Exercício 1
 const adicionaTurno = (objeto, chave, valor) => {
   objeto[chave]=valor
   //console.log(objeto)
   return objeto
 }
-
 adicionaTurno(lesson2,'turno', 'noite' )
 
+//Exercício 2
 const exibeChaves = (objeto)=> {
   //console.log(Object.keys(objeto))
   return Object.keys(objeto)
 }
-
 exibeChaves(lesson1)
 
+//Exercício 3
 const tamanhoDoObjeto = (objeto)=> {
   //console.log(Object.keys(objeto).length)
   return Object.keys(objeto).length
 }
-
 tamanhoDoObjeto(lesson1)
 
+//Exercício 4
 const valoresDoObjeto = (objeto)=> {
   //console.log(Object.values(objeto))
   return Object.values(objeto)
 }
-
 valoresDoObjeto(lesson1)
 
+//Exercício 5
 const allLessons= {lesson1:{}, lesson2:{}, lesson3:{}}
 
 Object.assign(allLessons.lesson1, lesson1)
@@ -55,6 +55,7 @@ Object.assign(allLessons.lesson3, lesson3)
 
 console.log(allLessons)
 
+//Exercício 6
 const totalDeAlunos = ()=>{
   const a = allLessons.lesson1['numeroEstudantes'];
   const b = allLessons.lesson2['numeroEstudantes'];
@@ -64,3 +65,24 @@ const totalDeAlunos = ()=>{
 }
 totalDeAlunos(allLessons)
 
+//Exercício 7
+const exibeValorPeloNumero = (objeto, numero) => {
+console.log(Object.values(objeto)[numero])
+Object.values(objeto)[numero]
+}
+exibeValorPeloNumero(lesson1, 0)
+
+//Exercício 8
+const verificaParChaveValor = (objeto, chave, valor) => {
+let chaves = Object.keys(objeto)
+let valores = Object.values(objeto)
+console.log(valores)
+// console.log(chaves)
+for(let i=0; i < chaves.length; i+=1 ){
+  if(chaves[i]===chave && valores[i]===valor){
+    return `Output: ${true}`
+  }
+}
+return `Output: ${false}`
+}
+verificaParChaveValor(lesson1, 'turno', 'manhã');
