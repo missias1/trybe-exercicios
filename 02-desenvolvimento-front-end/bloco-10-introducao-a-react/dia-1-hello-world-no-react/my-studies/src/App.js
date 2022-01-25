1fa306232299b2f4730f5ba699a6e793165d54c1
Leticia Missias <leticia.m.icm@gmail.com>
2022-01-25 11:38:07 -0300

    exercicios do dia 10.1

---
+++ 02-desenvolvimento-front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/my-studies/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Content from './Content.jsx';
+import Header from './Header.jsx';
+import Footer from './Footer.jsx';
+
+function App() {
+  return (
+<div>
+<Header/>
+<Content/>
+<Footer/>
+</div>
+  );
+}
+
+export default App;
